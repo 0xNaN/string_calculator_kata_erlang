46615f22b6b4e19991b83139707c6e83337a2f87
0xNaN <enzodicicco@gmail.com>
2015-11-16 15:31:57 +0100

    string 0 add to 0

--- src/calculator.erl
+++ src/calculator.erl
@@ -1,7 +1,8 @@
 -module(calculator).
 -export([add/1]).
 
-add("") -> 0.
+add("") -> 0;
+add("0")  -> 0.
 
 -ifdef(TEST).
 -include_lib("../test/calculator.hrl").
--- test/calculator.hrl
+++ test/calculator.hrl
@@ -2,3 +2,6 @@
 
 empty_string_should_add_to_0_test() ->
     ?assertEqual(0, calculator:add("")).
+
+string_0_should_add_to_0_test() ->
+    ?assertEqual(0, calculator:add("0")).
