f3448fb3175f348d313e4e6566cf7b80696d79c3
0xNaN <enzodicicco@gmail.com>
2015-11-16 15:40:03 +0100

    string N should add to N test

--- src/calculator.erl
+++ src/calculator.erl
@@ -1,8 +1,15 @@
 -module(calculator).
 -export([add/1]).
 
-add("") -> 0;
-add("0")  -> 0.
+add(String)  ->
+   to_number(String).
+
+to_number(String) ->
+    case string:to_integer(String) of
+        {error, no_integer} -> 0;
+        {Number, _} -> Number
+    end.
+
 
 -ifdef(TEST).
 -include_lib("../test/calculator.hrl").
--- test/calculator.hrl
+++ test/calculator.hrl
@@ -5,3 +5,16 @@ empty_string_should_add_to_0_test() ->
 
 string_0_should_add_to_0_test() ->
     ?assertEqual(0, calculator:add("0")).
+
+string_N_should_add_to_N_test() ->
+    ?assertEqual(1, calculator:add("1")),
+    ?assertEqual(2, calculator:add("2")),
+    ?assertEqual(3, calculator:add("3")),
+    ?assertEqual(4, calculator:add("4")),
+    ?assertEqual(5, calculator:add("5")),
+    ?assertEqual(6, calculator:add("6")),
+    ?assertEqual(7, calculator:add("7")),
+    ?assertEqual(8, calculator:add("8")),
+    ?assertEqual(9, calculator:add("9")),
+    ?assertEqual(10, calculator:add("10")),
+    ?assertEqual(125, calculator:add("125")).
